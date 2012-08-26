f3dda5944eb3869aa42631d95e089578a9d949f2
Stefan Wieczorek <stefan.wieczorek@weemedia.de>
2012-08-26 16:44:43 +0200

    initial commit

---
+++ data/symlinks.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+# find and deletes dead symlinks
+cd /var/www/magento1701/htdocs && find -L . -type l -delete
+
+lns -afFr /var/www/Mgt_AmazingWysiwyg/htdocs/ /var/www/magento1701/htdocs/
